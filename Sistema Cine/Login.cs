cc64fdee73270c90d64fa28cbfcbcae366763aef
raulito012 <raulquezada729@gmail.com>
2021-06-04 04:13:29 -0400

    Agregacion del  Dise;o del Login

--- Sistema Cine/Login.cs
+++ Sistema Cine/Login.cs
@@ -16,5 +16,25 @@ namespace Sistema_Cine.Login
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
